e0737023a4af4c9daab191c92315ad70760758e8
runningtalus <dave@davemay.org>
2015-06-27 09:55:46 -0600

    Testing different options with Vanilla JS and Flickity.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
-$(document).on('ready', function() {
-  console.log($);
+//$('.gallery-cell').flickity({
+//  autoPlay   : 3000,
+//  cellAlign  : 'left',
+//  contain    : true,
+//  freeScroll : true,
+//  lazyLoad   : true,
+//  pageDots   : true,
+//  percentPosition : false,
+//  prevNextButtons : false,
+//  wrapAround : true
+//});
+
+var elem;
+
+elem = document.querySelector('.gallery-cell');
+
+var flkty = new Flickity( elem, {
+  // options
+  accessibility: true,
+  autoPlay   : true,
+  cellAlign  : 'center',
+  contain    : true,
+  draggable: true,
+  freeScroll : true,
+  freeScrollFriction: 0.03,
+  imagesLoaded: true,
+  initialIndex: 3,
+  lazyLoad   : 2,
+  pageDots   : true,
+  percentPosition : true,
+  prevNextButtons : false,
+  resize: true,
+  rightToLeft: false,
+  setGallerySize : true,
+  wrapAround : true
 });
+
